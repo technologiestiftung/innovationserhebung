6bf07e6fc0e09ae996c6369fb01d3c186b9a4f1f
guadiromero <guadalupe.romero@ts.berlin>
2023-08-10 17:20:55 +0200

    bugfix: remove sinewave chart

--- app/sliders/pn_app.py
+++ app/sliders/pn_app.py
@@ -1,13 +1,12 @@
 import panel as pn
 from panel.layout.gridstack import GridSpec
 
-from .plotters import BarChart, BubbleChart, LineChart, SineWave, PieChart
+from .plotters import BarChart, BubbleChart, LineChart, PieChart
 
 
 def createApp():
     pc = PieChart()
     pc2 = PieChart()
-    sw = SineWave()
     bc = BubbleChart()
     lc = LineChart()
     bac = BarChart()
@@ -16,9 +15,8 @@ def createApp():
 
     gspec[0:1, 0:1] = pc.plot
     gspec[0:1, 1:2] = pc2.plot
-    gspec[1:2, 0:2] = sw.plot
-    gspec[2:3, 0:2] = bc.plot
-    gspec[3:4, 0:2] = lc.plot
-    gspec[4:5, 0:2] = bac.plot
+    gspec[1:2, 0:2] = bc.plot
+    gspec[2:3, 0:2] = lc.plot
+    gspec[3:4, 0:2] = bac.plot
 
     return gspec.servable()
