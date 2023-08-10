9e15054f3fe053bab674f8b2baf13c89ec8ea8c0
guadiromero <guadalupe.romero@ts.berlin>
2023-08-10 17:19:26 +0200

    feature: arrange plots in a grid layout

--- app/sliders/pn_app.py
+++ app/sliders/pn_app.py
@@ -1,8 +1,24 @@
 import panel as pn
+from panel.layout.gridstack import GridSpec
 
-from .sinewave import SineWave
+from .plotters import BarChart, BubbleChart, LineChart, SineWave, PieChart
 
 
 def createApp():
+    pc = PieChart()
+    pc2 = PieChart()
     sw = SineWave()
-    return pn.Row(sw.param, sw.plot).servable()
+    bc = BubbleChart()
+    lc = LineChart()
+    bac = BarChart()
+
+    gspec = GridSpec(width=800, height=1000)
+
+    gspec[0:1, 0:1] = pc.plot
+    gspec[0:1, 1:2] = pc2.plot
+    gspec[1:2, 0:2] = sw.plot
+    gspec[2:3, 0:2] = bc.plot
+    gspec[3:4, 0:2] = lc.plot
+    gspec[4:5, 0:2] = bac.plot
+
+    return gspec.servable()
