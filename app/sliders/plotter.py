34a553b7a0d8bfbd4012c4b857bbd9b26b2dc89b
guadiromero <guadalupe.romero@ts.berlin>
2023-08-31 16:09:00 +0200

    feat: add interactive line plot

--- app/sliders/plotter.py
+++ app/sliders/plotter.py
@@ -4,19 +4,39 @@ from math import pi
 import pandas as pd
 
 from bokeh.models import ColumnDataSource, ColorBar
-from bokeh.palettes import Category20c, Viridis256
+from bokeh.palettes import Category10, Category20c, Viridis256
 from bokeh.plotting import figure
 from bokeh.transform import cumsum, linear_cmap
+import panel
+
+
+PLOT_TYPES = {
+    "bar": "BarPlotter",
+    "bubble": "BubblePlotter",
+    "line": "LinePlotter",
+    "line_interactive": "InteractiveLinePlotter",
+    "pie": "PiePlotter",
+}
+
+
+class PlotterFactory:
+    @staticmethod
+    def create_plotter(plot_type, raw_data):
+        class_name = PLOT_TYPES[plot_type]
+        cls = globals()[class_name]
+        return cls(raw_data)
 
 
 class Plotter(ABC):
-    def __init__(self):
-        self.data = None
+    def __init__(self, raw_data):
+        self.raw_data = raw_data  # TODO: Refactor so input is already a DF (comes from Importer)
+        self.fitted_data = None
         self.plot = None
+        self.filters = None
         self.config = None
 
     @abstractmethod
-    def fit_data(self, data):
+    def fit_data(self):
         pass
 
     @abstractmethod
@@ -24,9 +44,187 @@ class Plotter(ABC):
         pass
 
 
+class BarPlotter(Plotter):
+    def __init__(self, raw_data):
+        # Create some example data
+        super().__init__(raw_data)
+
+        config = {
+            "general": {
+                "title": "Simple Bar Chart",
+                "x_axis_label": "Categories",
+                "y_axis_label": "Values"
+            },
+            "vbar": {
+                "width": 0.5,
+                "legend_label": "Values",
+                "line_color": "blue",
+                "fill_color": "blue"
+            },
+            "legend_title": "Legend",
+            "label_text_font_size": "12pt"
+        }
+
+        self.config = config
+
+    def fit_data(self):
+        self.fitted_data = ColumnDataSource(data=self.raw_data)
+
+    def create_plot(self):
+        # Create the figure
+        self.plot = figure(x_range=self.fitted_data.data["x"], **self.config["general"])
+
+        # Add vertical bars to the figure using the ColumnDataSource
+        self.plot.vbar(x="x", top="y", source=self.fitted_data, **self.config["vbar"])
+
+        # Show the legend
+        self.plot.legend.title = self.config["legend_title"]
+        self.plot.legend.label_text_font_size = self.config["label_text_font_size"]
+
+
+class BubblePlotter(Plotter):
+    def __init__(self, raw_data):
+        super().__init__(raw_data)
+
+        config = {
+            "figure": {
+                "title": "Simple Bubble Plot",
+                "toolbar_location": None
+            },
+            "linear_cmap": {
+                "field_name": "color",
+                "palette": Viridis256
+            },
+            "circle": {
+                "legend_field": "color"
+            },
+            "color_bar": {
+                "width": 8,
+                "location": (0, 0)
+            }
+        }
+
+        self.config = config
+
+    def fit_data(self):
+        source = ColumnDataSource(data={"x": self.raw_data["x"],
+                                        "y": self.raw_data["y"],
+                                        "size": self.raw_data["size"],
+                                        "color": self.raw_data["color"]})
+        self.fitted_data = source
+
+    def create_plot(self):
+        # Create the figure
+        self.plot = figure(**self.config["figure"])
+
+        # Add circles to the plot
+        mapper = linear_cmap(field_name="color", palette=Viridis256, low=min(self.fitted_data.data["color"]), high=max(self.fitted_data.data["color"]))
+        self.plot.circle(x="x", y="y", size="size", color=mapper, source=self.fitted_data, **self.config["circle"])
+
+        # Add color bar
+        color_bar = ColorBar(color_mapper=mapper["transform"], **self.config["color_bar"])
+        self.plot.add_layout(color_bar, "right")
+
+
+class LinePlotter(Plotter):
+    def __init__(self, raw_data):
+        super().__init__(raw_data)
+
+        config = {
+            "general": {
+                "title": "Simple Line Chart",
+                "x_axis_label": "X-axis",
+                "y_axis_label": "Y-axis"
+            },
+            "line": {
+                "line_width": 2,
+                "legend_label": "Line",
+                "line_color": "blue"
+            },
+            "legend_title": "Legend",
+            "label_text_font_size": "12pt"
+        }
+
+        self.config = config
+
+    def fit_data(self):
+        # Create a ColumnDataSource for the data
+        self.fitted_data = ColumnDataSource(data=self.raw_data)
+
+    def create_plot(self):
+        # Create the figure
+        self.plot = figure(**self.config["general"])
+
+        # Add a line glyph to the figure
+        self.plot.line(x="x", y="y", source=self.fitted_data, **self.config["line"])
+
+        # Show the legend
+        self.plot.legend.title = self.config["legend_title"]
+        self.plot.legend.label_text_font_size = self.config["label_text_font_size"]
+
+
+# TODO: Set a fixed scale so it doesn't shrink/grow automatically with updates
+class InteractiveLinePlotter(Plotter):
+    def __init__(self, raw_data):
+        super().__init__(raw_data)
+
+        config = {
+            "general": {
+                "title": "Interactive Line Chart",
+                "x_axis_label": "X-axis",
+                "y_axis_label": "Y-axis"
+            },
+            "line": {
+                "line_width": 2,
+                "legend_label": "Line",
+                "line_color": "blue"
+            },
+            "legend_title": "Legend",
+            "label_text_font_size": "12pt",
+            "initial_lines": ["line1", "line2", "line3"]
+        }
+
+        self.config = config
+
+    def fit_data(self):
+        self.raw_data = pd.DataFrame(self.raw_data)
+
+        # Create widgets
+        self.filters = panel.widgets.MultiChoice(
+            name='Select Lines', options=self.config["initial_lines"]
+        )
+
+        # Create initial data source
+        initial_data = self.raw_data[['x'] + self.config["initial_lines"]]
+        self.fitted_data = ColumnDataSource(initial_data)
+
+    def create_plot(self):
+        # Create a Bokeh figure
+        self.plot = figure(**self.config["general"])
+
+        # Add lines to the plot
+        colors = Category10[10]
+        for i, line_name in enumerate(self.config["initial_lines"]):
+            self.plot.line(x='x', y=line_name, source=self.fitted_data, color=colors[i], legend_label=line_name)
+
+        # Add interactivity
+        self.filters.param.watch(self.update, 'value')
+
+    def update(self, event):
+        # Define the callback function for the filter
+        selected_lines = self.filters.value
+        filtered_df = self.raw_data[['x'] + selected_lines]
+
+        # Update the existing ColumnDataSource with new data
+        self.fitted_data.data = {
+            'x': filtered_df['x'],
+            **{line_name: filtered_df[line_name] for line_name in selected_lines}
+        }
+
+
 class PiePlotter(Plotter):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, raw_data):
+        super().__init__(raw_data)
 
         config = {
             "general": {
@@ -54,149 +252,22 @@ class PiePlotter(Plotter):
 
         self.config = config
 
-    def fit_data(self, data):
+    def fit_data(self):
 
-        df = pd.DataFrame(data)
+        df = pd.DataFrame(self.raw_data)
         df["angle"] = df["y"] / df["y"].sum() * 2 * pi
-        df["color"] = Category20c[len(data["x"])]
+        df["color"] = Category20c[len(self.raw_data["x"])]
 
         source = ColumnDataSource(df)
 
-        self.data = source
+        self.fitted_data = source
 
     def create_plot(self):
         plot = figure(**self.config["general"])
 
-        plot.wedge(**self.config["wedge"], source=self.data)
+        plot.wedge(**self.config["wedge"], source=self.fitted_data)
         plot.axis.axis_label = self.config["axis_label"]
         plot.axis.visible = self.config["visible"]
         plot.grid.grid_line_color = self.config["grid_line_color"]
 
         self.plot = plot
-
-
-class BubblePlotter(Plotter):
-    def __init__(self):
-        super().__init__()
-
-        config = {
-            "figure": {
-                "title": "Simple Bubble Plot",
-                "toolbar_location": None
-            },
-            "linear_cmap": {
-                "field_name": "color",
-                "palette": Viridis256
-            },
-            "circle": {
-                "legend_field": "color"
-            },
-            "color_bar": {
-                "width": 8,
-                "location": (0, 0)
-            }
-        }
-
-        self.config = config
-
-    def fit_data(self, data):
-        source = ColumnDataSource(data={"x": data["x"],
-                                        "y": data["y"],
-                                        "size": data["size"],
-                                        "color": data["color"]})
-        self.data = source
-
-    def create_plot(self):
-        # Create the figure
-        self.plot = figure(**self.config["figure"])
-
-        # Add circles to the plot
-        mapper = linear_cmap(field_name="color", palette=Viridis256, low=min(self.data.data["color"]), high=max(self.data.data["color"]))
-        self.plot.circle(x="x", y="y", size="size", color=mapper, source=self.data, **self.config["circle"])
-
-        # Add color bar
-        color_bar = ColorBar(color_mapper=mapper["transform"], **self.config["color_bar"])
-        self.plot.add_layout(color_bar, "right")
-
-
-class LinePlotter(Plotter):
-    def __init__(self):
-        super().__init__()
-
-        config = {
-            "general": {
-                "title": "Simple Line Chart",
-                "x_axis_label": "X-axis",
-                "y_axis_label": "Y-axis"
-            },
-            "line": {
-                "line_width": 2,
-                "legend_label": "Line",
-                "line_color": "blue"
-            },
-            "legend_title": "Legend",
-            "label_text_font_size": "12pt"
-        }
-
-        self.config = config
-
-    def fit_data(self, data):
-        # Create a ColumnDataSource for the data
-        self.data = ColumnDataSource(data=data)
-
-    def create_plot(self):
-        # Create the figure
-        self.plot = figure(**self.config["general"])
-
-        # Add a line glyph to the figure
-        self.plot.line(x="x", y="y", source=self.data, **self.config["line"])
-
-        # Show the legend
-        self.plot.legend.title = self.config["legend_title"]
-        self.plot.legend.label_text_font_size = self.config["label_text_font_size"]
-
-
-class BarPlotter(Plotter):
-    def __init__(self):
-        # Create some example data
-        super().__init__()
-
-        config = {
-            "general": {
-                "title": "Simple Bar Chart",
-                "x_axis_label": "Categories",
-                "y_axis_label": "Values"
-            },
-            "vbar": {
-                "width": 0.5,
-                "legend_label": "Values",
-                "line_color": "blue",
-                "fill_color": "blue"
-            },
-            "legend_title": "Legend",
-            "label_text_font_size": "12pt"
-        }
-
-        self.config = config
-
-    def fit_data(self, data):
-        self.data = ColumnDataSource(data=data)
-
-    def create_plot(self):
-        # Create the figure
-        self.plot = figure(x_range=self.data.data["x"], **self.config["general"])
-
-        # Add vertical bars to the figure using the ColumnDataSource
-        self.plot.vbar(x="x", top="y", source=self.data, **self.config["vbar"])
-
-        # Show the legend
-        self.plot.legend.title = self.config["legend_title"]
-        self.plot.legend.label_text_font_size = self.config["label_text_font_size"]
-
-
-class PlotterFactory:
-    @staticmethod
-    def create_plotter(plot_type):
-        class_name = plot_type.capitalize() + "Plotter"
-        cls = globals()[class_name]
-        return cls()
--- app/sliders/pn_app.py
+++ app/sliders/pn_app.py
@@ -22,6 +22,13 @@ line_data = {
     "y": [6, 7, 2, 4, 5]
 }
 
+interactive_line_data = {
+    'x': [1, 2, 3, 4, 5],
+    'line1': [5, 8, 6, 10, 7],
+    'line2': [3, 5, 9, 6, 4],
+    'line3': [8, 4, 2, 7, 9]
+}
+
 bar_data = {"x": ["A", "B", "C", "D"],
             "y": [15, 40, 25, 30]
 }
@@ -30,24 +37,28 @@ bar_data = {"x": ["A", "B", "C", "D"],
 def create_app():
     plotter_factory = PlotterFactory()
 
-    pie_plotter = plotter_factory.create_plotter("pie")
-    pie_plotter.fit_data(pie_data)
+    pie_plotter = plotter_factory.create_plotter("pie", pie_data)
+    pie_plotter.fit_data()
     pie_plotter.create_plot()
 
-    pie_plotter_2 = plotter_factory.create_plotter("pie")
-    pie_plotter_2.fit_data(pie_data)
+    pie_plotter_2 = plotter_factory.create_plotter("pie", pie_data)
+    pie_plotter_2.fit_data()
     pie_plotter_2.create_plot()
 
-    bubble_plotter = plotter_factory.create_plotter("bubble")
-    bubble_plotter.fit_data(bubble_data)
+    bubble_plotter = plotter_factory.create_plotter("bubble", bubble_data)
+    bubble_plotter.fit_data()
     bubble_plotter.create_plot()
 
-    line_plotter = plotter_factory.create_plotter("line")
-    line_plotter.fit_data(line_data)
+    interactive_line_plotter = plotter_factory.create_plotter("line_interactive", interactive_line_data)
+    interactive_line_plotter.fit_data()
+    interactive_line_plotter.create_plot()
+
+    line_plotter = plotter_factory.create_plotter("line", line_data)
+    line_plotter.fit_data()
     line_plotter.create_plot()
 
-    bar_plotter = plotter_factory.create_plotter("bar")
-    bar_plotter.fit_data(bar_data)
+    bar_plotter = plotter_factory.create_plotter("bar", bar_data)
+    bar_plotter.fit_data()
     bar_plotter.create_plot()
 
     gspec = GridSpec(width=800, height=1000)
@@ -55,7 +66,7 @@ def create_app():
     gspec[0:1, 0:1] = pie_plotter.plot
     gspec[0:1, 1:2] = pie_plotter_2.plot
     gspec[1:2, 0:2] = bubble_plotter.plot
-    gspec[2:3, 0:2] = line_plotter.plot
-    gspec[3:4, 0:2] = bar_plotter.plot
+    gspec[2:3, 0:2] = interactive_line_plotter.plot
+    gspec[3:4, 0:2] = interactive_line_plotter.filters
 
     return gspec.servable()
