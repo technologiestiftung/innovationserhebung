665c441b38a632d5ccc587c20bdaa2bbfa65eb2c
Urs Schmidt <urs.schmidt@ts.berlin>
2023-09-11 14:49:08 +0200

    feat: charts get renderd in individual sections

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.gzip import GZipMiddleware
 import panel as pn
-from sliders.pn_app import create_app
+from sliders.pn_app import get_base_chart, get_funky_bubble_chart, get_pizza_chart, create_app
 from utils.translation import load_translation
 from enum import Enum
 import logging
@@ -26,11 +26,21 @@ async def bkapp_page(request: Request, language: Language = None):
     if language is None:
         language = request.headers.get("accept-language", "de")
     translations = load_translation(language)
+
+    request.app.extra["pizza_chart"] = server_document('http://127.0.0.1:5000/pizza_chart')
+    request.app.extra["base_chart"] = server_document('http://127.0.0.1:5000/base_chart')
+    request.app.extra["funky_bubble_chart"] = server_document('http://127.0.0.1:5000/funky_bubble_chart')
+
     script = server_document('http://127.0.0.1:5000/app')
-    return templates.TemplateResponse("index.html", {"request": request, "script": script, "translations": translations})
+    return templates.TemplateResponse("index.html", {"request": request, "script": script, "translations": translations})   
 
 
 
-pn.serve({'/app': create_app},
+pn.serve({
+    '/app': create_app, 
+    '/pizza_chart': get_pizza_chart, 
+    '/base_chart': get_base_chart, 
+    '/funky_bubble_chart': get_funky_bubble_chart
+    },
         port=5000, allow_websocket_origin=["127.0.0.1:8000"],
          address="127.0.0.1", show=False)
