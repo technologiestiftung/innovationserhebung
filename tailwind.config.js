ca249358bdca0c1435af51e52ba1686b700d1736
Urs Schmidt <urs.schmidt@ts.berlin>
2023-09-20 16:41:16 +0200

    feat: styled hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       colors: {
         "corporate-blue": "#1E3791",
       },
+      margin: {
+        "60vh": "60vh",
+      },
+      boxShadow: {
+        top: "0px -8px 24px 0px rgba(0, 0, 0, 0.32)",
+      },
     },
   },
   plugins: [],
