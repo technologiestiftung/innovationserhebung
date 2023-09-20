f3174f9495ec43afc8ae67a7e9bc3be12230ad12
Urs Schmidt <urs.schmidt@ts.berlin>
2023-09-20 16:56:58 +0200

    feat: hero responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,26 @@ module.exports = {
   content: ["app/templates/**/*.html"],
   theme: {
     extend: {
+      boxShadow: {
+        top: "0px -8px 24px 0px rgba(0, 0, 0, 0.32)",
+      },
       colors: {
         "corporate-blue": "#1E3791",
       },
       margin: {
         "60vh": "60vh",
+        hero: "var(--width-hero)",
       },
-      boxShadow: {
-        top: "0px -8px 24px 0px rgba(0, 0, 0, 0.32)",
+      maxWidth: {
+        hero: "var(--width-hero)",
+      },
+      padding: {
+        mobile: "1.5rem",
+        desktop: "4.75rem",
+      },
+      screens: {
+        desktop: "1280px",
+        tablet: "768px",
       },
     },
   },
