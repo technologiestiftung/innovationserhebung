b59ea5556d440d242bbb8817db53d1921f03a5c4
Urs Schmidt <urs.schmidt@ts.berlin>
2023-09-21 10:44:03 +0200

    chores: open browser on server start

--- run_dev.sh
+++ run_dev.sh
@@ -6,4 +6,8 @@ uvicorn main:app --reload &
 
 cd ..
 # Start the Tailwind CSS watcher from the app/tailwindcss folder
-tailwindcss -i app/styles/main.css -o app/static/css/main.css --watch
+tailwindcss -i app/styles/main.css -o app/static/css/main.css --watch & 
+
+xdg-open http://127.0.0.1:8000
+open http://127.0.0.1:8000
+start http://127.0.0.1:8000
