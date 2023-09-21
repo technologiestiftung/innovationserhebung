b6bbe7c77cd22ca000d7565adc70e390f31b4cac
Urs Schmidt <urs.schmidt@ts.berlin>
2023-09-21 13:34:23 +0200

    chores: better global styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["app/templates/**/*.html"],
-  theme: {
-    extend: {
-      boxShadow: {
-        top: "0px -8px 24px 0px rgba(0, 0, 0, 0.32)",
-      },
-      colors: {
-        "corporate-blue": "#1E3791",
-      },
-      margin: {
-        "60vh": "60vh",
-        hero: "var(--width-hero)",
-      },
-      maxWidth: {
-        hero: "var(--width-hero)",
-      },
-      padding: {
-        mobile: "1.5rem",
-        desktop: "4.75rem",
-      },
-      screens: {
-        desktop: "1280px",
-        tablet: "768px",
-      },
+    content: ["app/templates/**/*.html"],
+    jit: true,
+    theme: {
+        extend: {
+            boxShadow: {
+                top: "0px -8px 24px 0px rgba(0, 0, 0, 0.32)",
+            },
+            colors: {
+                "corporate-blue": "#1E3791",
+            },
+            margin: {
+                "60vh": "60vh",
+                hero: "var(--width-hero)",
+            },
+            maxWidth: {
+                hero: "var(--width-hero)",
+            },
+            padding: {
+                mobile: "1.5rem",
+                desktop: "4.75rem",
+            },
+            screens: {
+                desktop: "1280px",
+                tablet: "768px",
+            },
+            fontSize: {
+                h1: "2rem",
+                h2: "1.125rem",
+            },
+            lineHeight: {
+                h1: "2.5rem",
+                h2: "1.5rem",
+            },
+        },
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
