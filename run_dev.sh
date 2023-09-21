a2664515764f8c6b6f9551120f8d876a05087d3a
Urs Schmidt <urs.schmidt@ts.berlin>
2023-09-21 13:34:23 +0200

    chores: better global styling

--- run_dev.sh
+++ run_dev.sh
@@ -4,10 +4,9 @@
 cd app
 uvicorn main:app --reload &
 
+python -m webbrowser -t "http://127.0.0.1:8000" & 
+
 cd ..
 # Start the Tailwind CSS watcher from the app/tailwindcss folder
-tailwindcss -i app/styles/main.css -o app/static/css/main.css --watch & 
+tailwindcss -i app/styles/main.css -o app/static/css/main.css --watch
 
-xdg-open http://127.0.0.1:8000
-open http://127.0.0.1:8000
-start http://127.0.0.1:8000
