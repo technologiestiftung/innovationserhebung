f91cd1e479cbcca779a7445b86f7eea1a07a6cda
Urs Schmidt <urs.schmidt@ts.berlin>
2023-10-16 11:32:31 +0200

    feat: use html tags in translations

--- app/utils/translation.py
+++ app/utils/translation.py
@@ -4,12 +4,23 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
+
+HTML_TAGS = {
+    "<strong>": "\u003cstrong\u003e",
+    "</strong>": "\u003c/strong\u003e",
+    "<br>": "\u003cbr/\u003e",
+}
+
 def load_translation(language):
     primary_language = language.split(",")[0].split(";")[0].split("-")[0]
     translations = {}
     try:
         with open(f"locales/{primary_language}.json", "r", encoding="utf-8") as f:
             translations = json.load(f)
+            for key in translations:
+                if isinstance(translations[key], str):
+                    for tag, escape_sequence in HTML_TAGS.items():
+                        translations[key] = translations[key].replace(tag, escape_sequence)
     except FileNotFoundError:
         pass
     logger.info(f"Loaded translations for {language}")
