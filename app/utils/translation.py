5c6de1ca2108713193f619e3b42dfd0c9bbfc485
Sebastian Herrmann <sebastian.herrmann@ts.berlin>
2023-10-26 13:42:29 +0200

    refactor: comment out obsolete(?) translation HTML parsing

--- app/utils/translation.py
+++ app/utils/translation.py
@@ -5,11 +5,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-HTML_TAGS = {
-    "<strong>": "\u003cstrong\u003e",
-    "</strong>": "\u003c/strong\u003e",
-    "<br>": "\u003cbr/\u003e",
-}
+# HTML_TAGS = {
+#     "<strong>": "\u003cstrong\u003e",
+#     "</strong>": "\u003c/strong\u003e",
+#     "<br>": "\u003cbr/\u003e",
+# }
 
 def load_translation(language):
     primary_language = language.split(",")[0].split(";")[0].split("-")[0]
@@ -17,10 +17,11 @@ def load_translation(language):
     try:
         with open(f"locales/{primary_language}.json", "r", encoding="utf-8") as f:
             translations = json.load(f)
-            for key in translations:
-                if isinstance(translations[key], str):
-                    for tag, escape_sequence in HTML_TAGS.items():
-                        translations[key] = translations[key].replace(tag, escape_sequence)
+            # TODO: Remove this if it is no longer needed.
+            # for key in translations:
+            #     if isinstance(translations[key], str):
+            #         for tag, escape_sequence in HTML_TAGS.items():
+            #             translations[key] = translations[key].replace(tag, escape_sequence)
     except FileNotFoundError:
         pass
     logger.info(f"Loaded translations for {language}")
