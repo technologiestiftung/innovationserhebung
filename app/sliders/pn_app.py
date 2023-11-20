931e5a70e8295b7b0061ce5847bfb50d2e7a6a2e
Urs Schmidt <urs.schmidt@ts.berlin>
2023-11-20 15:56:16 +0100

    feat/chart_toggle from panel select

--- app/sliders/pn_app.py
+++ app/sliders/pn_app.py
@@ -1,7 +1,9 @@
 import json
+import param
 
 from panel.layout.gridstack import GridSpec
 from panel.layout.flex import FlexBox
+from panel.widgets import Select
 
 from .config_importer import ConfigImporter
 from .plotter import PlotterFactory
@@ -50,6 +52,9 @@ interactive_line_data = {
 with open("data/outfile.json", "r") as f:
     data = json.load(f)
 
+with open("locales/de.json", "r") as f:
+    content = json.load(f)
+
 # Load config
 config_importer = ConfigImporter()
 config = config_importer.get_config()
@@ -70,7 +75,8 @@ for plot_key in config:
         plot_data = data[plot_key]
 
     # TODO: Can refactor create_plotter() so I only need to pass the config as an argument
-    plotter = plotter_factory.create_plotter(config[plot_key]["plot_type"], plot_data, config[plot_key])
+    plotter = plotter_factory.create_plotter(
+        config[plot_key]["plot_type"], plot_data, config[plot_key])
     plotter.generate()
 
     plotters[plot_key] = plotter
@@ -83,15 +89,31 @@ def get_base_chart():
                                                               config["base_line_interactive"])
     interactive_line_plotter.generate()
 
-    line_plotter = plotter_factory.create_plotter("line", line_data, config["base_line_interactive"])
+    line_plotter = plotter_factory.create_plotter(
+        "line", line_data, config["base_line_interactive"])
     line_plotter.generate()
 
-    base_chart = GridSpec(sizing_mode="stretch_both", min_height=800)
+    select = Select(options={
+        'Deutschland': 'de', 'Berlin': 'ber'}, value='de')
 
-    base_chart[0:3, 0:2] = interactive_line_plotter.plot["ber"]
-    base_chart[3:6, 0:2] = interactive_line_plotter.plot["de"]
-    base_chart[6:7, 0:1] = interactive_line_plotter.filters_multi_choice
-    base_chart[6:7, 1:2] = interactive_line_plotter.filters_single_choice
+    plotted_base_charts = {
+        'de': interactive_line_plotter.plot['de'],
+        'ber': interactive_line_plotter.plot['ber'],
+    }
+
+    base_chart = GridSpec(sizing_mode="stretch_width", min_height=800)
+
+    base_chart[0:1, 0:1] = select
+    base_chart[1:8, 0:4] = plotted_base_charts[select.value]
+    base_chart[8:12, 0:2] = interactive_line_plotter.filters_multi_choice
+    base_chart[8:12, 2:4] = interactive_line_plotter.filters_single_choice
+
+    def update_chart(event):
+        selected_location = event.new
+        base_chart[1:8, 0:4] = None
+        base_chart[1:8, 0:4] = plotted_base_charts[selected_location]
+
+    select.param.watch(update_chart, 'value')
 
     return base_chart
 
@@ -103,15 +125,31 @@ chart_collection = {}
 
 plot_key = "fue_pie_interactive"
 plotter = plotters[plot_key]
+plotted_charts = {
+    'de': plotter.plot["de"],
+    'ber': plotter.plot["ber"],
+}
 
-flex_obj = FlexBox(plotter.plot["ber"],
-                   plotter.plot["de"],
-                   plotter.filters_single_choice,
-                   plotter.filters_single_choice_highlight,
-                   flex_direction="column",
-                   align_items="center",
-                   sizing_mode="stretch_width")
-chart_collection[plot_key] = flex_obj
+location_toggle = Select(options={
+    'Deutschland': 'de', 'Berlin': 'ber'}, value='de')
+
+
+flex_obj_1 = FlexBox(location_toggle,
+                     plotted_charts[location_toggle.value],
+                     plotter.filters_single_choice,
+                     plotter.filters_single_choice_highlight,
+                     flex_direction="column",
+                     align_items="center",
+                     sizing_mode="stretch_width")
+chart_collection[plot_key] = flex_obj_1
+
+
+def update_chart(event):
+    selected_location = event.new
+    flex_obj_1.__setitem__(1, plotted_charts[selected_location])
+
+
+location_toggle.param.watch(update_chart, 'value')
 
 plot_key = "shares_pie_interactive"
 plotter = plotters[plot_key]
