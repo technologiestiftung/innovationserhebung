3b7057e6acaf678e440e14115e99b463c51f0b31
Urs Schmidt <urs.schmidt@ts.berlin>
2023-12-12 16:51:40 +0100

    chores: refactor update chart function

--- app/plot/layout.py
+++ app/plot/layout.py
@@ -17,32 +17,39 @@ config = config_importer.get_config()
 
 # Initialize plotter factory
 plotter_factory = PlotterFactory()
-
 chart_collection = {}
+
+
+def update_chart(flex_obj, plotter):
+    """
+    Update chart on toggle selection by user.
+
+    :param flex_obj: FlexBox object
+    :param plotter: Plotter object
+    :return: callable function
+    """
+    return lambda event: flex_obj.__setitem__(1, plotter.plots[event.new])
+
+
 for plot_key in config:
     # Generate plots and filters
     plotter = plotter_factory.create_plotter(data[plot_key], config[plot_key])
     plotter.generate()
 
-    location_toggle = Select(options={
-        'Deutschland': 'de', 'Berlin': 'ber'}, value='de')
-
-    def create_update_chart(flex_obj, plotter):
-        def update_chart(event):
-            selected_location = event.new
-            flex_obj.__setitem__(1, plotter.plots[selected_location])
-        return update_chart
+    location_toggle = Select(options={"Deutschland": "de", "Berlin": "ber"}, value="de")
 
     # Place plots and filters in the layout
-    flex_obj = FlexBox(location_toggle,
-                       #    *plotter.plots.values(),
-                       plotter.plots[location_toggle.value],
-                       *plotter.filters.values(),
-                       flex_direction="column",
-                       align_items="center",
-                       sizing_mode="stretch_width")
+    flex_obj = FlexBox(
+        location_toggle,
+        #    *plotter.plots.values(),
+        plotter.plots[location_toggle.value],
+        *plotter.filters.values(),
+        flex_direction="column",
+        align_items="center",
+        sizing_mode="stretch_width",
+    )
 
-    update_chart = create_update_chart(flex_obj, plotter)
-    location_toggle.param.watch(update_chart, 'value')
+    # Connect toggle to the show display so that it changes on user selection
+    location_toggle.param.watch(update_chart(flex_obj, plotter), "value")
 
     chart_collection[plot_key] = flex_obj
