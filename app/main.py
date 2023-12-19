3cc3b9aa69bbc5a60540cd87237affbfdbacf41f
Matthieu Rigal <matthieu.rigal@ts.berlin>
2023-12-19 15:47:41 +0100

    fix: pass again to avoid exceptions on quit

--- app/main.py
+++ app/main.py
@@ -147,7 +147,7 @@ if PROXY_PANEL_THROUGH_FASTAPI:
                         data = await ws_server.recv()
                         await ws_client.send_text(data)
                 except ConnectionClosedOK:
-                    await ws_client.close()
+                    pass
 
             # Sync the upstream and downstream websockets through asyncio
             await asyncio.gather(listen_to_client(), listen_to_server())
